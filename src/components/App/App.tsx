d5608e160e453fda4f348e7a754a525adee93203
NFabien <fabien.nicola@oclock.school>
2024-05-14 15:35:11 +0200

    dependencies: add semantic to package.json

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,17 @@
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 import Signin from '../Signin/Signin';
 import './App.scss';
-import Login from '../Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <Signin></Signin>
+      <Signin />
       <Login />
       <Footer />
-
     </div>
   );
 }
