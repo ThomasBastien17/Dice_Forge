c16172c559eead1dfea844920ac022f8c32f4b4a
LauraGautier <lc.gautier@icloud.com>
2024-05-14 14:37:48 +0200

    Homepage V2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,10 @@
 import './App.scss';
-import Header from '../Header/Header';
 import Home from '../Home/Home';
-import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Home />
-      <Footer />
     </div>
   );
 }
