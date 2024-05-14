e3e2de525352ec7239fc4fbbbb4fb226b57c875e
Thomas Bastien <thomas.bastien@oclock.school>
2024-05-14 23:14:50 +0200

    fix: css page signin

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,65 +1,53 @@
-import { Label, Input } from 'semantic-ui-react';
+import { Form, FormInput, Button } from 'semantic-ui-react';
 import './Signin.scss';
 
 function Signin() {
   return (
     <div className="signin">
       <h1 className="signin-title">Inscription</h1>
-      <form className="signin-form">
-        <Label as={Input} className="signin-label">
-          Avatar
-          <Input type="file" />
-        </Label>
-        <Label className="signin-label">
-          Nom
-          <Input
-            type="text"
-            id="lastname"
-            name="lastname"
-            placeholder="Entrez votre nom"
-          />
-        </Label>
-        <label htmlFor="firsname" className="signin-label">
-          Prenom
-        </label>
-        <input
+      <Form className="signin-form">
+        <FormInput
+          className="signin-input"
+          icon="user"
+          iconPosition="left"
+          label="Nom"
+          placeholder="Nom"
+        />
+        <FormInput
+          className="signin-input"
+          icon="user"
+          iconPosition="left"
+          label="Prénom"
           type="text"
-          id="firstname"
-          name="firstname"
-          placeholder="Entrez votre prenom"
+          placeholder="Prénom"
         />
-        <label htmlFor="email" className="signin-label">
-          Email:
-        </label>
-        <input
+        <FormInput
+          className="signin-input"
+          icon="lock"
+          iconPosition="left"
+          label="Email"
           type="email"
-          id="email"
-          pattern=".+@example\.com"
-          size={30}
-          required
-          placeholder="Entrez votre Email : exemple@email.com"
+          placeholder="Email"
         />
-        <label htmlFor="pass" className="signin-label">
-          Mot de passe :
-        </label>
-        <input
+        <FormInput
+          className="signin-input"
+          icon="lock"
+          iconPosition="left"
+          label="Mot de passe"
           type="password"
-          id="pass"
-          name="password"
-          required
-          placeholder="Votre mot de passe"
+          placeholder="Mot de passe"
         />
-        <label htmlFor="pass-confirm" className="signin-label">
-          Confirmation :
-        </label>
-        <input
+        <FormInput
+          className="signin-input"
+          icon="lock"
+          iconPosition="left"
+          label="Confirmation"
           type="password"
-          id="pass-confirm"
-          name="password-confirm"
-          required
-          placeholder="confirmez votre mot de passe"
+          placeholder="Confirmation mot de passe"
         />
-      </form>
+
+        <Button className="signin-btn" content="Valider" primary />
+      </Form>
     </div>
   );
 }
