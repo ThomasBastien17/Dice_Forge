2d938d1078911282d67009c5d2ab193c49bf67dc
NFabien <fabien.nicola@oclock.school>
2024-05-15 23:47:13 +0200

    feat/signup adding api call to post user datas

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { useState } from 'react';
 import CreateGame from '../CreateGame/CreateGame';
 import Game from '../Game/Game';
 import Home from '../Home/Home';
@@ -9,25 +8,12 @@ import Signup from '../Signup/Signup';
 import './App.scss';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/signup"
-          element={
-            <Signup
-              email={email}
-              setEmail={setEmail}
-              password={password}
-              setPassword={setPassword}
-            />
-          }
-        />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/creategame" element={<CreateGame />} />
         <Route path="/game" element={<Game />} />
       </Routes>
