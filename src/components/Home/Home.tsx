c58d438a6a74985a7da138ed0e7e2a5d0a7d59d2
LauraGautier <lc.gautier@icloud.com>
2024-05-15 14:52:17 +0200

    New route for CreateGame

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable react/no-unescaped-entities */
 import './Home.scss';
 import 'semantic-ui-css/semantic.min.css';
-import Header from '../Header/Header';
 import { Image, Container, Button, Icon } from 'semantic-ui-react';
+import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 function Home() {
@@ -22,9 +21,10 @@ function Home() {
           <p className="home-paragraph">Bienvenue sur Dice Forge !</p>
           <p className="home-paragraph">
             Votre plateforme ultime pour la gestion de parties de jeux de rôles
-            en ligne ! Que vous soyez un maître du donjon expérimenté ou un
-            aventurier débutant, notre site offre tout ce dont vous avez besoin
-            pour rendre vos aventures encore plus épiques.
+            en ligne ! <br />
+            Que vous soyez un maître du donjon expérimenté ou un aventurier
+            débutant, notre site offre tout ce dont vous avez besoin pour rendre
+            vos aventures encore plus épiques.
           </p>
           <p className="home-paragraph">
             Plongez dans un univers de possibilités infinies, où chaque lancer
