c9470f7a3e52c40f09516f98bea0f72163ba22a1
solremi <jeremie.rocquet@outlook.fr>
2024-05-29 10:56:04 +0200

    maj

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://roje6147.odns.fr/api',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
