6377b2f02993735336937928325614790305cb53
Thomas Bastien <thomas.bastien@oclock.school>
2024-05-29 15:05:06 +0200

    feat: add a input control read and write

--- src/components/Game/EditGame.tsx
+++ src/components/Game/EditGame.tsx
@@ -2,8 +2,14 @@ import { Form, FormInput, FormSelect, FormTextArea, Input } from "semantic-ui-re
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import './EditGame.scss';
+import { useState } from "react";
 
 function EditGame() {
+  const [game, setGame] = useState('');
+  const [date, setDate] = useState('');
+  const [note, setNote] = useState('');
+  const [music, setMusic] = useState('');
+
   return (
     <div className="edit-game">
       <Header />
@@ -11,11 +17,19 @@ function EditGame() {
         <h1 className="edit-game-title">Réglage de la partie</h1>
         <Form className="edit-game-form">
           <FormInput className="edit-game-input" label="Nom de la partie" placeholder="partie 1" icon="game"
-          iconPosition="left"/>
-          <FormInput className="edit-game-input" label="Date de session" placeholder="22/06/24" icon="calendar alternate" iconPosition="left"  />
-          <FormTextArea className="edit-game-input" placeholder="Ajouter vos notes pour la partie" label="Notes" />
+          iconPosition="left" value={game} onChange={(event) => {
+            setGame(event.target.value);
+          }}/>
+          <FormInput className="edit-game-input" label="Date de session" placeholder="22/06/24" icon="calendar alternate" iconPosition="left" value={date} onChange={(event) => {
+            setDate(event.target.value);
+          }} />
+          <FormTextArea className="edit-game-input" placeholder="Ajouter vos notes pour la partie" label="Notes" value={note} onChange={(event) => {
+            setNote(event.target.value);
+          }} />
           <label>Musique
-          <input className="edit-game-input" type="file"></input>
+          <input className="edit-game-input" type="file" value={music} onChange={(event) => {
+            setMusic(event.target.value);
+          }}></input>
           </label>
         </Form>
       </div>
