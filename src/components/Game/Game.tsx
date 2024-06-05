8b28690fca9879954359c7cda16cf4898d667d8e
NFabien <fabien.nicola@oclock.school>
2024-06-05 15:21:11 +0200

    removing log

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -51,14 +51,11 @@ function Game() {
   }, []);
 
   useEffect(() => {
-    console.log('Valeur de gameId:', gameId);
-
     const getGameById = async () => {
       try {
         const response = await axiosInstance.get(`/game/${gameId}`);
         if (response.status === 200) {
           setGame(response.data);
-          console.log('reponse du axios game', response);
         }
       } catch (error) {
         console.error('erreur lors de la récupération du jeu', error);
